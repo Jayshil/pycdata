7563454b745d5e4fe014dda9cde18250af164916
Jayshil <jayshilpatel147@gmail.com>
2021-11-25 19:22:22 +0530

    Can now tackle PIPE data

--- pycdata/dpr.py
+++ pycdata/dpr.py
@@ -27,7 +27,7 @@ def tess_data(name, pdc=True, verbose=True):
         Default is True
     -----------
     return
-        tar.gz file readable to
+        tgz file readable to
         pycheops
     """
     try:
@@ -92,7 +92,7 @@ def tess_data(name, pdc=True, verbose=True):
         mjd1 = bjd1 - 2400000.
         status, event, dark, conta, conta_err, smear, smerr, roll, locx, locy =\
             np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl),\
-            np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.ones(len(fl))*np.floor(cenx[0]), np.ones(len(fl))*np.floor(ceny[0])
+            np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl)
         # Creating astropy Table for storing data
         tab = Table()
         tab['UTC_TIME'], tab['MJD_TIME'], tab['BJD_TIME'], tab['FLUX'], tab['FLUXERR'], tab['STATUS'],\
@@ -127,6 +127,95 @@ def tess_data(name, pdc=True, verbose=True):
         os.system('rm ' + name2 + '.fits')
         os.system('mv ' + name1 + '.tgz ' + p3 + '/' + name1 + '.tgz')
         if verbose:
+            print('----------------------------------------------------------------------------------------')
             print('Name\t\tTIC-id\t\t\tSector\t\t\t.tgz name')
             print('----------------------------------------------------------------------------------------')
             print(name + '\t\t' + ticids[i][8:] + '\t\t' + sectors[i][-4:] + '\t\t' + name1)
+
+
+def pipe_data(name, fileid, imgette=True, flag=0):
+    """
+    Parameters:
+    -----------
+    name : str
+        name of the fits file
+        containing the PIPE data
+    fileid : str
+        unique file id for the target visit
+    imegette : bool
+        Boolean on whether the data comes
+        from imegette or sub-array
+        Default is True.
+    -----------
+    return
+        tgz file readable to
+        pycheops
+    """
+    fileid = fileid[:-6]
+    hdul = fits.open(name)
+    hdr = hdul[1].header
+    dta = Table.read(hdul[1])
+    # Masking datasets
+    flg = np.asarray(dta['FLAG'])                 # Flagged data
+    msk = np.where(flg==0)[0]                     # Creating a mask to remove flg>0 values
+    # Extracting data from file
+    mjd1, bjd1 = np.asarray(dta['MJD_TIME'])[msk], np.asarray(dta['BJD_TIME'])[msk]                 # Various time formats stored
+    bjd2 = Time(bjd1, format='jd')                                                                  # Converting to UTC time
+    utc1 = bjd2.to_value('fits')
+    # Storing Data
+    fl, fle = np.asarray(dta['FLUX'])[msk], np.asarray(dta['FLUXERR'])[msk]
+    roll, xc, yc, tft2 = np.asarray(dta['ROLL'])[msk], np.asarray(dta['XC'])[msk],\
+        np.asarray(dta['YC'])[msk], np.asarray(dta['thermFront_2'])[msk]
+    if imgette:
+        bg = np.asarray(dta['BG'])[msk] * 50 * 50
+    else:
+        bg = np.asarray(dta['BG'])[msk] * 200 * 200
+    # For relative weights of the first arbitrary PSF principal components
+    Us_n = np.array([])      # To store the names of U
+    Us = []                  # To store the values of U
+    cols = dta.colnames      # Name of all columns
+    for j in range(len(cols)):
+        if cols[j][0] == 'U':
+            Us_n = np.hstack((Us_n, cols[j]))
+    for j in range(len(Us_n)):
+        usn = np.asarray(dta[Us_n[j]])[msk]
+        Us.append(usn)
+    # Making an arrays of zero for other elements
+    status, event, dark, conta, conta_err, smear, smerr, locx, locy =\
+        np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl),\
+        np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl), np.zeros_like(fl)
+    # Creating an astropy table for storing the data
+    tab = Table()
+    tab['UTC_TIME'], tab['MJD_TIME'], tab['BJD_TIME'], tab['FLUX'], tab['FLUXERR'], tab['STATUS'],\
+        tab['EVENT'], tab['DARK'], tab['BACKGROUND'], tab['CONTA_LC'], tab['CONTA_LC_ERR'], tab['SMEARING_LC'],\
+        tab['SMEARING_LC_ERR'], tab['ROLL_ANGLE'], tab['LOCATION_X'], tab['LOCATION_Y'], tab['CENTROID_X'], tab['CENTROID_Y'] =\
+        utc1, mjd1, bjd1, fl, fle, status, event, dark, bg, conta, conta_err, smear, smerr, roll, locx, locy, xc, yc
+    for j in range(len(Us_n)):
+        tab[Us_n[j]] = Us[j]
+    # Saving the table first
+    tab.write(fileid + '_V0000_PIPE.fits', format='fits')
+    # Making changes to the saved file
+    tb_fits = fits.open(fileid + '_V0000_PIPE.fits')
+    tb_fits_hdr = tb_fits[1].header
+    hdr1 = hdr[8:]
+    hdr2 = hdr1[:-47]
+    crds = hdr2.cards
+    for j in range(len(crds)):
+        tb_fits_hdr.append(crds[j])
+    tb_fits_hdr.append(('AP_RADI', 0.0, '(px) Radius of Aperture/NA -- PSF Photometry'))                 # Aperture radius
+    nn1 = tb_fits_hdr['TARGNAME']
+    os.system('rm ' + fileid + '_V0000_PIPE.fits')
+    tb_fits.writeto(fileid + '_SCI_COR_Lightcurve-DEFAULT_V0000_PIPE.fits')
+    os.system('tar -cvzf ' + fileid + '_V0000_PIPE.tgz ' + fileid + '_SCI_COR_Lightcurve-DEFAULT_V0000_PIPE.fits')
+    os.system('mv ' + fileid + '_V0000_PIPE.tgz ' + p3 + '/' + fileid + '_V0000_PIPE.tgz')
+    os.system('rm ' + fileid + '_SCI_COR_Lightcurve-DEFAULT_V0000_PIPE.fits')
+    print('-----------------------------------------------------------------------------')
+    print('Name of the file\t\t\t\t.tgz file')
+    print('-----------------------------------------------------------------------------')
+    print(nn1 + '\t\t\t\t' + fileid + '_V0000_PIPE.tgz')
+    # For meta files
+    tab1 = Table()
+    tab1['thermFront_2'] = tft2
+    tab1.write(fileid + '-meta.fits')
+    os.system('mv ' + fileid + '-meta.fits ' + p3 + '/' + fileid + '_V0000-meta.fits')
+    print('meta\t\t\t\t\t' + fileid + '_V0000-meta.fits')
